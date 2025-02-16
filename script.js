4f53b2abb6e1c96ce3b07d53c88e873bd1824b61
Ajibona <39883461+ajibona@users.noreply.github.com>
2025-02-16 14:46:06 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuClicked = document.querySelectorAll('.js__mobile--menu')
+
+const mobileNav = document.querySelector('.js__mobile--nav')
+
+menuClicked.forEach((btn)=>{
+  btn.addEventListener('click', ()=>{
+    mobileNav.classList.toggle('mobile__activated')
+  })
+})
+
+
+console.log(menuClicked)
